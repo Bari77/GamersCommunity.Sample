c40941c5b1e66568436377a7b6da5255daddbeec
Bari <alex.c9777700@gmail.com>
2025-10-02 17:41:48 +0200

    Add tests and bump

---
+++ Sample.Tests/TestsFixture.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Consumer.Services;
+using Sample.Database.Context;
+
+namespace Sample.Tests
+{
+    public class TestsFixture : IDisposable
+    {
+        #region Global
+
+        /// <summary>
+        /// Testing database context
+        /// </summary>
+        public readonly SampleDbContext _testDbContext;
+
+        #region Implementations
+
+        #region Countries
+
+        /// <summary>
+        /// Implementation for users
+        /// </summary>
+        public readonly UsersService _usersService;
+
+        #endregion
+
+        #endregion
+
+        #endregion
+
+        /// <summary>
+        /// Called when tests start
+        /// /!\ CARE: This context is shared between tests /!\
+        /// </summary>
+        public TestsFixture()
+        {
+            var options = new DbContextOptionsBuilder<SampleDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _testDbContext = new SampleDbContext(options);
+
+            // Init all implementations
+            _usersService = new UsersService(_testDbContext);
+        }
+
+        /// <summary>
+        /// Called when test is ended
+        /// </summary>
+        public void Dispose()
+        {
+            // Clean
+        }
+    }
+}
---
+++ Sample.Tests/Users/UsersTests.cs
@@ -0,0 +1,43 @@
+﻿using GamersCommunity.Core.Tests;
+using Sample.Consumer.Services;
+using Sample.Database.Context;
+using Sample.Database.Models;
+
+namespace Sample.Tests.Users
+{
+    public class UsersTests : GenericServiceTests<SampleDbContext, UsersService, User>, IClassFixture<TestsFixture>
+    {
+        public UsersTests(TestsFixture fixture) : base(fixture._usersService)
+        {
+            fixture._testDbContext.AddRange(GetFakeData());
+            fixture._testDbContext.SaveChanges();
+        }
+
+        protected override List<User> GetFakeData()
+        {
+            return
+            [
+                new() {
+                    Id = 1,
+                    CreationDate = DateTime.Now,
+                    ModificationDate = DateTime.Now,
+                    Name = "Jane Doe",
+                },
+                new() {
+                    Id = 2,
+                    CreationDate = DateTime.Now,
+                    ModificationDate = DateTime.Now,
+                    Name = "John Doe",
+                },
+            ];
+        }
+
+        protected override User GetNewEntity()
+        {
+            return new User
+            {
+                Name = "New user"
+            };
+        }
+    }
+}
